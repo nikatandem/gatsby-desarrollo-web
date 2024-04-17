5be02b0f587eb32089b2efeb3eb6fc47dbd94c09
nikatandem <nikatandem@gmail.com>
2024-04-17 09:57:36 +0200

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+import React from "react"
+import { Link } from "gatsby"
+import "../styles.css"
+
+const Footer = () => {
+  return (
+  <div>
+    <p className="footer"> &copy; Innovation Co. 2024</p>
+  </div>
+  )
+}
+
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,3 @@
-// Header.js
-
 import React from "react"
 import { Link } from "gatsby"
 import "../styles.css"
